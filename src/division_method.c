86c1ab483c4e4fa3a52f01a0f8eda8ef1ece549b
José Ángel López <jose.a.l.e@hotmail.com>
2024-05-12 17:56:56 +0200

    feature(dmhash): add initial implementation

--- src/division_method.c
+++ src/division_method.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <string.h>
+
+#define PRIME_NUMBER 71
+
+static int hash_division_method(const char*);
+
+static int hash_division_method(const char* input) {
+	int hash_number = 0;
+	int input_length = strlen(input);
+
+	for (int i = 0; i < input_length; i++) {
+		hash_number += (int)input[i];
+	}
+
+	return hash_number % PRIME_NUMBER;
+}
 
 int main() {
-	printf("Hello World!");
+	char user_input[256] = "";
+	printf("Hash Division Method\n");
+	printf("--------------------\n");
+	printf("Input String: %s", user_input);
+	fgets(user_input, sizeof(user_input), stdin);
+	user_input[strcspn(user_input, "\n")] = 0;
+	printf("Hash Value: %d\n", hash_division_method(user_input));
 	return 0;
 }
